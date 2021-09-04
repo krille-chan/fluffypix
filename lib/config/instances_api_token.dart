0fb0428b13b53583e57a73cbc92a762a1442e944
Christian Pauly <christian-pauly@posteo.de>
2021-09-04 15:53:10 +0200

    chore: Hide api key

--- lib/config/instances_api_token.dart
+++ lib/config/instances_api_token.dart
@@ -1,4 +1,3 @@
 abstract class InstancesApiToken {
-  static const String token =
-      'HutRbSQRAvhqe2T8hw4bTQUquUP0O7mD4JpBJvU858AInl5zmYDN8sLJ5Ges2hdLxCVhgXXox2hjSeaCNc7mJuy32gzvP10fhR65aQ9gqvC9KlNqgvUx5Hvrv6b6FsqM';
+  static const String token = '';
 }
