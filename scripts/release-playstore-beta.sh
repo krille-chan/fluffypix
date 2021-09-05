9138de40e5038bcb3580a3047fb81caf447d6133
Christian Pauly <christian-pauly@posteo.de>
2021-09-05 08:31:40 +0200

    fix: Android release

--- scripts/release-playstore-beta.sh
+++ scripts/release-playstore-beta.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-flutter channel stable
-flutter upgrade
+flutter clean
 flutter pub get
 flutter build appbundle --target-platform android-arm,android-arm64,android-x64
 mkdir -p build/android
