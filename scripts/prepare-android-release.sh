5d3918dc9dbcd7298f75e580409b8f656a246925
Christian Pauly <christian-pauly@posteo.de>
2021-09-05 08:16:44 +0200

    feat: Fastlane for Android

--- scripts/prepare-android-release.sh
+++ scripts/prepare-android-release.sh
@@ -5,9 +5,8 @@ echo "storePassword=${FDROID_KEY_PASS}" >> key.properties
 echo "keyPassword=${FDROID_KEY_PASS}" >> key.properties
 echo "keyAlias=key" >> key.properties
 echo "storeFile=../key.jks" >> key.properties
-#echo $PLAYSTORE_DEPLOY_KEY >> keys.json
-#bundle install
-#bundle update fastlane
-#bundle exec fastlane set_build_code_internal
-#echo $GOOGLE_SERVICES >> app/google-services.json
+echo $PLAYSTORE_DEPLOY_KEY >> keys.json
+bundle install
+bundle update fastlane
+bundle exec fastlane set_build_code_internal
 cd ..
