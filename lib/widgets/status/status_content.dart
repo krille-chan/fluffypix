7abef5380d4beae6007eb3b33e749bdd206f59be
Christian Pauly <christian-pauly@posteo.de>
2021-09-06 10:11:16 +0200

    refactor: Rename text status content

--- lib/widgets/status/status_content.dart
+++ lib/widgets/status/status_content.dart
@@ -6,7 +6,7 @@ import 'package:simple_html_css/simple_html_css.dart';
 import 'package:fluffypix/config/app_configs.dart';
 import 'package:fluffypix/model/status.dart';
 import 'package:fluffypix/utils/links_callback.dart';
-import 'package:fluffypix/widgets/status/image_status_content.dart';
+import 'package:fluffypix/widgets/status/text_status_content.dart';
 import 'package:fluffypix/widgets/status/sensitive_content.dart';
 import 'package:fluffypix/widgets/status/status_content_slider.dart';
 
